19a137bd45becf1124f71f1c55324fe8ca4e8bf7
Lenny Bogdonoff <rememberlenny@gmail.com>
2015-12-16 16:19:22 -0500

    Save any contact for msubmissions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,9 @@ class PagesController < ApplicationController
   skip_authorization_check
   skip_before_action :authenticate_user!
   def home
-
+    @contact = Contact.new
   end
+
   # Preview html email template
   def email
     tpl = (params[:layout] || 'hero').to_sym
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,8 +8,8 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
-      :to => "your_email@example.org",
+      :subject => "ReplyFrom: Contact Form Submission",
+      :to => "lkbgift@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
