562d5b707fb3f8a43e4242e706fcbd8c52253152
rememberlenny <rememberlenny@gmail.com>
2015-12-25 23:31:16 -0500

    Save the PLANT THE FLAG

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,10 +2,12 @@ class Email < ActiveRecord::Base
   after_create :check_account
 
   def check_account
+    puts 'CHECKACCOUNT GO'
     account = Account.where(slug: self.to.split('@')[0]).first
     if account && !account.empty?
       self.account_id = account.id
       self.save
     end
+    puts 'CHECKACCOUNT STOP'
   end
 end
