c04b21e84b90ea072b2c354658c2734bebb91c5a
rememberlenny <rememberlenny@gmail.com>
2015-12-25 23:25:22 -0500

    Save the PLANT THE FLAG

--- app/models/email.rb
+++ app/models/email.rb
@@ -4,7 +4,8 @@ class Email < ActiveRecord::Base
   def check_account
     account = Account.where(slug: self.to.split('@')[0]).first
     if account && !account.empty?
-
+      self.account_id = account.id
+      self.save
     end
   end
 end
