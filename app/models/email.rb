f96e30c65d6f0ff6928ee1783dc28f68530aead3
rememberlenny <rememberlenny@gmail.com>
2015-12-27 16:53:35 -0500

    Set up the check fro receiving

--- app/models/email.rb
+++ app/models/email.rb
@@ -8,7 +8,7 @@ class Email < ActiveRecord::Base
     to_email = self.to
     slug = to_email.split('@')[0]
     account = Account.where(slug: slug).first
-    if !account.is_receiving
+    if !account.is_receiving && self.from == ENV['mail.from']
       account.is_receiving = true
       account.save
     end
