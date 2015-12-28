122242861ba84fd7d519b4c3e4d12a5896d56a6d
rememberlenny <rememberlenny@gmail.com>
2015-12-27 21:01:11 -0500

    Change the email verification

--- app/models/email.rb
+++ app/models/email.rb
@@ -8,7 +8,7 @@ class Email < ActiveRecord::Base
     to_email = self.to
     slug = to_email.split('@')[0]
     account = Account.where(slug: slug).first
-    if !account.is_receiving && self.from == ENV['mail.from']
+    if !account.is_receiving && self.from.split('@')[1] == ENV['MAIL_HOST']
       account.is_receiving = true
       account.save
     end
