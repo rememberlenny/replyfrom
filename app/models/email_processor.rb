db3ab9170e92dbd6e4d60145a99dc94b80b3f398
Lenny Bogdonoff <rememberlenny@gmail.com>
2015-12-31 11:02:24 -0500

    Save the RF

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -10,18 +10,34 @@ class EmailProcessor
       original_to = to
       to = @email.headers["X-Forwarded-To"]
     end
-    Email.create(
-      to: to,
-      original_to: original_to,
-      from: @email.from[:email],
-      subject: @email.subject,
-      raw_text: @email.raw_text,
-      raw_html: @email.raw_html,
-      raw_body: @email.raw_body,
-      body: @email.body,
-      headers: @email.headers.to_json,
-      # forwarded: @email["X-Forwarded-To"],
-    )
+    if to
+      email_params['to'] = to
+    end
+    if original_to
+      email_params['original_to'] = original_to
+    end
+    if @email.from[:email]
+      email_params['from'] = @email.from[:email]
+    end
+    if @email.subject
+      email_params['subject'] = @email.subject
+    end
+    if @email.raw_text
+      email_params['raw_text'] = @email.raw_text
+    end
+    if @email.raw_html
+      email_params['raw_html'] = @email.raw_html
+    end
+    if @email.raw_body
+      email_params['raw_body'] = @email.raw_body
+    end
+    if @email.body
+      email_params['body'] = @email.body
+    end
+    if @email.headers.to_json
+      email_params['headers'] = @email.headers.to_json
+    end
+    Email.create(email_params)
     # pry
     puts "Done"
   end
